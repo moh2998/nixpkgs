35470fd3bdd699fde56c7d0052c26fb8748c149a
Frank Lanitz <fl@flyingcircus.io>
2018-08-13 15:34:19 +0200

    Activate mod_proxy_fcgi for Apache

--- nixos/modules/flyingcircus/roles/apache.nix
+++ nixos/modules/flyingcircus/roles/apache.nix
@@ -42,6 +42,7 @@ in
     services.httpd.adminAddr = "nobody@example.com";
     services.httpd.multiProcessingModule = "worker";
     services.httpd.port = serverStatusPort;
+    services.httpd.extraModules = [ "proxy_fcgi" ];
 
     services.httpd.extraConfig = ''
       ExtendedStatus on
