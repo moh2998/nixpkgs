12ac4612582d11b674c9e8702be6206d6e48baf1
Frank Lanitz <fl@flyingcircus.io>
2018-08-24 12:56:12 +0200

    Backport PostGis 2.4.4 from current master

--- nixos/modules/flyingcircus/packages/postgis/default.nix
+++ nixos/modules/flyingcircus/packages/postgis/default.nix
@@ -1,97 +1,93 @@
-args@{fetchurl, composableDerivation, stdenv, perl, libxml2, postgresql, geos, proj, flex, gdal, json_c, pkgconfig, file, ...}:
+{ fetchurl
+, stdenv
+, perl
+, libxml2
+, postgresql
+, geos
+, proj
+, gdal
+, json_c
+, pkgconfig
+, file
+}:
 
   /*
+  ### NixOS - usage:
+  ==================
+    services.postgresql.extraPlugins = [ (pkgs.postgis.override { postgresql = pkgs.postgresql95; }) ];
   ### important Postgis implementation details:
   =============================================
-
     Postgis provides a shared library implementing many operations. They are
     exposed to the Postgres SQL interpreter by special SQL queries eg:
-
       CREATE FUNCTION [...]
               AS '[..]liblwgeom', 'lwhistogram2d_in' LANGUAGE 'C' IMMUTABLE STRICT; -- WITH (isstrict);
-
    where liblwgeom is the shared library.
    Postgis < 1.5 used absolute paths, in NixOS $libdir is always used.
-
    Thus if you want to use postgresql dumps which were created by non NixOS
    systems you have to adopt the library path.
+   ### TODO:
+   =========
+   the bin commands to have gtk gui:
   */
 
 
 let
-  pgDerivationBase = composableDerivation.composableDerivation {} ( fix :
+  version = "2.4.4";
+  sha256 = "1hm8migjb53cymp4qvg1h20yqllmy9f7x0awv5450391i6syyqq6";
+in stdenv.mkDerivation rec {
+  name = "postgis-${version}";
 
-  let version = fix.fixed.version; in {
+  src = fetchurl {
+    url = "https://download.osgeo.org/postgis/source/postgis-${builtins.toString version}.tar.gz";
+    inherit sha256;
+  };
 
-    name = "postgis-${version}";
+  # don't pass these vars to the builder
+  removeAttrs = ["sql_comments" "sql_srcs"];
 
-    src = fetchurl {
-      url = "http://postgis.refractions.net/download/postgis-${fix.fixed.version}.tar.gz";
-      inherit (fix.fixed) sha256;
-    };
+  preInstall = ''
+    mkdir -p $out/bin
+  '';
 
-    # don't pass these vars to the builder
-    removeAttrs = ["hash" "sql_comments" "sql_srcs"];
+  # create aliases for all commands adding version information
+  postInstall = ''
+    sql_srcs=$(for sql in ${builtins.toString sql_srcs}; do echo -n "$(find $out -iname "$sql") "; done )
+    for prog in $out/bin/*; do # */
+      ln -s $prog $prog-${version}
+    done
+    cp -r doc $out
+  '';
 
-    preConfigure = ''
-      configureFlags="--datadir=$out/share --datarootdir=$out/share --bindir=$out/bin"
-      makeFlags="PERL=${perl}/bin/perl datadir=$out/share pkglibdir=$out/lib bindir=$out/bin"
-    '';
+  buildInputs = [ libxml2 postgresql geos proj perl gdal json_c pkgconfig ];
 
-    # create aliases for all commands adding version information
-    postInstall = ''
+  sql_comments = "postgis_comments.sql";
 
-      sql_srcs=$(for sql in ${builtins.toString fix.fixed.sql_srcs}; do echo -n "$(find $out -iname "$sql") "; done )
+  sql_srcs = ["postgis.sql" "spatial_ref_sys.sql"];
 
-      for prog in $out/bin/*; do # */
-        ln -s $prog $prog-${version}
-      done
+  # postgis config directory assumes /include /lib from the same root for json-c library
+  # NIX_LDFLAGS = "-L${stdenv.lib.getLib json_c}/lib";
 
-      cp -r doc $out
-    '';
+  dontDisableStatic = true;
+  preConfigure = ''
+    sed -i 's@/usr/bin/file@${file}/bin/file@' configure
+    configureFlags="--datadir=$out/share --datarootdir=$out/share --bindir=$out/bin --with-gdalconfig=${gdal}/bin/gdal-config --with-jsondir=${json_c}"
+    makeFlags="PERL=${perl}/bin/perl datadir=$out/share pkglibdir=$out/lib bindir=$out/bin"
+  '';
+  postConfigure = ''
+    sed -i "s|@mkdir -p \$(DESTDIR)\$(PGSQL_BINDIR)||g ;
+            s|\$(DESTDIR)\$(PGSQL_BINDIR)|$prefix/bin|g
+            " \
+        "raster/loader/Makefile";
+    sed -i "s|\$(DESTDIR)\$(PGSQL_BINDIR)|$prefix/bin|g
+            " \
+        "raster/scripts/python/Makefile";
+  '';
 
-    buildInputs = [libxml2 postgresql geos proj perl];
-
-    sql_comments = "postgis_comments.sql";
-
-    meta = {
-      description = "Geographic Objects for PostgreSQL";
-      homepage = "http://postgis.refractions.net";
-      license = stdenv.lib.licenses.gpl2;
-      maintainers = [stdenv.lib.maintainers.marcweber];
-      platforms = stdenv.lib.platforms.linux;
-    };
-  });
-
-  pgDerivationBaseNewer = pgDerivationBase.merge (fix: {
-    src = fetchurl {
-      url = "http://download.osgeo.org/postgis/source/postgis-${builtins.toString fix.fixed.version}.tar.gz";
-      inherit (fix.fixed) sha256;
-    };
-  });
-
-in
-pgDerivationBaseNewer.merge (fix: {
-    version = "2.4.4";
-    sha256 = "0663efb589210d5048d95c817e5cf29552ec8180e16d4c6ef56c94255faca8c2";
-    sql_srcs = ["postgis.sql" "spatial_ref_sys.sql"];
-    builtInputs = [gdal json_c pkgconfig];
-
-    # postgis config directory assumes /include /lib from the same root for json-c library
-    NIX_LDFLAGS = "-L${json_c}/lib";
-
-    dontDisableStatic = true;
-    preConfigure = ''
-      sed -i 's@/usr/bin/file@${file}/bin/file@' configure
-      configureFlags="$configureFlags --with-gdalconfig=${gdal}/bin/gdal-config --with-jsondir=${json_c}"
-    '';
-    postConfigure = ''
-      sed -i "s|@mkdir -p \$(DESTDIR)\$(PGSQL_BINDIR)||g ;
-              s|\$(DESTDIR)\$(PGSQL_BINDIR)|$prefix/bin|g
-              " \
-          "raster/loader/Makefile";
-      sed -i "s|\$(DESTDIR)\$(PGSQL_BINDIR)|$prefix/bin|g
-              " \
-          "raster/scripts/python/Makefile";
-    '';
-  })
+  meta = with stdenv.lib; {
+    description = "Geographic Objects for PostgreSQL";
+    homepage = http://postgis.refractions.net;
+    license = licenses.gpl2;
+    maintainers = [ maintainers.marcweber ];
+    platforms = platforms.linux;
+  };
+}
